0fef80b8a8e3a9a4edd2a49d5241e05cf7f52e2b
Michal Jan Warecki <mwarecki1@gmail.com>
2023-05-16 22:09:50 +0200

    Sjå "changelog"

--- scripts/boot.py
+++ scripts/boot.py
@@ -4,7 +4,8 @@ confs = glob.glob("/srv/portfolio/*/apache/dev.conf")
 
 for conf in confs:
   parts = conf.split("/")
-  project = parts[1]
+  project = parts[-3]
   newconf = "/srv/portfolio/apache/" + project + ".conf"
 
+  print("Copying", conf, "\t=>\t", newconf)
   subprocess.run(["cp", conf, newconf])
