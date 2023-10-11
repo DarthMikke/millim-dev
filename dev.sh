870e59dc11d68239aaaea4babcbada541da43648
Michal Jan Warecki <mwarecki1@gmail.com>
2023-10-11 19:27:06 +0200

    Use ENV vars for local volume paths (#2)

--- dev.sh
+++ dev.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env sh
-if [ -z `cat ./portfolio-path 2> /dev/null` ]
-then
-echo "portfolio-path er tom."
+if [[ ! -z `cat .env.dev 2> /dev/null` ]]; then
+source .env.dev set
+fi
+
+if [[ -z "${SERVE_DIR}" ]]; then
+echo "SERVE_DIR not set"
 exit 1
 fi
 
-docker run -d -p 80:80/tcp -v `cat ./portfolio-path`:/srv/portfolio millim-dev
+if [[ -z "${CONF_DIR}" ]]; then
+echo "SERVE_DIR not set"
+exit 1
+fi
+
+docker run -d -p 80:80/tcp -v "${SERVE_DIR}:/srv/portfolio" -v "${CONF_DIR}:/srv/apache" millim-dev
