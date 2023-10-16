940378b2f209746aa85e70039d7d059bc77fa745
Michal Jan Warecki <mwarecki1@gmail.com>
2023-10-16 22:48:21 +0200

    Moved git setup to runtime instead of buildtime.

--- Dockerfile
+++ Dockerfile
@@ -2,23 +2,13 @@ FROM httpd:2.4-alpine
 
 RUN apk add openssh git python3 nodejs apache2-mod-wsgi
 
-ARG SERVE_GIT
-ARG SERVE_BRANCH=dev
-ARG CONF_GIT
-ARG CONF_BRANCH=dev
-
 # Kopier tilgangsnøkkelen til Git-repoet
 RUN mkdir /root/.ssh
-COPY ./ssh/* /root/.ssh/
-RUN ssh-keyscan -t rsa github.com >> /root/.ssh/known_hosts
+COPY ./ssh/config /root/.ssh/
+RUN mkdir /ssh
+COPY ./ssh/*.key /ssh
+VOLUME [ "/ssh" ]
 
-# Lagre git-repoet på eit persistent volum
-RUN git clone ${SERVE_GIT} /srv/portfolio
-RUN cd /srv/portfolio && git checkout dev
-
-# Lagre git-repoet på eit persistent volum
-RUN git clone ${CONF_GIT} /srv/apache
-RUN cd /srv/apache && git checkout dev
 VOLUME [ "/srv" ]
 
 # Inkluder httpd-konfigurasjonen frå git-repoet
@@ -28,5 +18,3 @@ RUN echo "LoadModule wsgi_module /usr/lib/apache2/mod_wsgi.so" >> /usr/local/apa
 COPY ./scripts/* /scripts/
 RUN chmod +x /scripts/*
 CMD /scripts/run.sh
-
-EXPOSE 22
--- scripts/boot.py
+++ scripts/boot.py
@@ -1,2 +1,85 @@
 #!/usr/bin/env python3
 import os
+import subprocess
+import time
+
+# Check if the key is uploaded
+if not os.path.exists("/ssh/github.key"):
+    print("Place the private key in /ssh/github.key")
+    print("Shutting down in 10 minutes.")
+    time.sleep(600)
+    exit(10)
+else:
+    print("Found the private key.")
+
+# Add github to .known_hosts
+
+print("Add github to .known_hosts")
+try:
+    with open("/root/.ssh/known_hosts", 'x') as fh:
+        res = subprocess.run(
+            ["ssh-keyscan", "-t", "rsa", "github.com"],
+            stdout=fh
+        )
+    res.check_returncode()
+except Exception as e:
+    print(e)
+    exit(11)
+
+PORTFOLIO = '/srv/portfolio'
+CONFIG = '/srv/apache'
+
+# Check if the served files dir exists
+if not os.path.exists(f"{PORTFOLIO}/.git"):
+    print("Portfolio directory does not exist, setting up.")
+    try:
+        res = subprocess.run(
+            ["git", "clone", os.environ['SERVE_GIT'], PORTFOLIO]
+        )
+        res.check_returncode()
+    except Exception as e:
+        print(e)
+        exit(1)
+
+os.chdir(PORTFOLIO)
+print(f"Checking out branch {os.environ['SERVE_BRANCH']}.")
+try:
+    res = subprocess.run(
+        ["git", "checkout", os.environ['SERVE_BRANCH']])
+    res.check_returncode()
+except Exception as e:
+    print(e)
+    exit(2)
+
+print(f"Initiating submodules.")
+try:
+    res = subprocess.run(
+        ["git", "submodule", "update", "--init", "--recursive"]
+    )
+    res.check_returncode()
+except Exception as e:
+    print(e)
+    exit(5)
+
+
+# Check if the configs dir exists
+if not os.path.exists(f"{CONFIG}/.git"):
+    print("Config directory does not exist, setting up.")
+    try:
+        res = subprocess.run(
+            ["git", "clone", os.environ['CONF_GIT'], "/srv/apache"]
+        )
+        res.check_returncode()
+    except Exception as e:
+        print(e)
+        exit(3)
+
+os.chdir("/srv/apache")
+print(f"Checking out branch {os.environ['CONF_BRANCH']}.")
+try:
+    res = subprocess.run(
+        ["git", "checkout", os.environ['CONF_BRANCH']])
+    res.check_returncode()
+except Exception as e:
+    print(e)
+    exit(4)
