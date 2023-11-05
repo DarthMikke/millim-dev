d109bfd5786437c297ade3aaa17fa41f2748f62e
Michal Jan Warecki <mwarecki1@gmail.com>
2023-11-05 23:11:32 +0100

    Setup script

--- scripts/setup.py
+++ scripts/setup.py
@@ -9,9 +9,19 @@ PORTFOLIO = '/srv/portfolio'
 
 
 def locate_setup(project_path):
-    filenames = [x for x in glob.glob(os.path.)
+    """
+    Locate a setup script in the project directory. The script might either
+    be located in the project root, or `scripts` directory inside the project.
+    """
+    patterns = ['setup', 'setup*', 'scripts/setup', 'scripts/setup*']
+    filenames = [glob.glob(project_path + "/" + p) for p in patterns]
+    flattened = filenames[0]
+    for fn in filenames:
+        flattened += fn
+    filenames = [x for x in flattened
                  if os.path.isfile(x)]
-    
+    if len(filenames) > 0:
+        return filenames[0]
 
 
 projects = [x for x in glob.glob(f"{PORTFOLIO}/*")
@@ -19,7 +29,16 @@ projects = [x for x in glob.glob(f"{PORTFOLIO}/*")
 
 for project_path in projects:
     project = os.path.basename(project_path)
-    print(f"Setting up {project}...")
-    res = subprocess.run(
-        [f"{project_path}/scripts/setup"],
-    )
+    setup = locate_setup(project_path)
+    if setup:
+        interpreter = None
+        (_, ext) = os.path.splitext(setup)
+        # TODO: Executable from the shebang
+        command = ['sh', '-c'] if ext == 'bash' else ['python'] \
+            if ext == 'py' else []
+        command.append(setup)
+        print("$ " + ' '.join(command))
+        os.chdir(project_path)
+        res = subprocess.run(
+            command
+        )
